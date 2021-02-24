770ce479bcc1378c3a4ce4a4d57fffb9c04da2bb
Anatoliy <doroshenkoaa@gmail.com>
2021-02-24 21:36:34 +0200

    Добавил комментарий для git^)

--- lesson_2.3_switch_to.window.py
+++ lesson_2.3_switch_to.window.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 def calc(x):
     return str(math.log(abs(12 * math.sin(x))))
 
+# комментарий для GIT
+
 
 link = "http://suninjuly.github.io/redirect_accept.html"
 browser = webdriver.Chrome()
